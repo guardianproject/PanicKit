7287d439631fd544d57c652d944fe6a6b21a5272
Hans-Christoph Steiner <hans@eds.org>
2015-10-29 12:54:39 +0100

    rework storing "Connected" state
    This works on android-10 and makes "Connected" state purely a matter of
    what is stored in the app's panic preferences.  This also switches to using
    separate prefs file for panic prefs to keep this simple.

--- src/info/guardianproject/panic/PanicTrigger.java
+++ src/info/guardianproject/panic/PanicTrigger.java
@@ -1,19 +1,24 @@
-
 package info.guardianproject.panic;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PanicTrigger {
 
-    public static final String PREF_RECEIVER_PACKAGE_NAMES = "panicReceiverPackageNames";
+    private static final String SHARED_PREFS = "info.guardianproject.panic.PanicTrigger";
+    private static final int SHARED_PREFS_MODE = Context.MODE_PRIVATE;
+    private static final String CONNECTED = "connected";
 
     /**
      * Checks whether the provided {@link Activity} was started with the action
@@ -54,11 +59,9 @@ public class PanicTrigger {
      * @return whether it was successfully completed
      */
     public static boolean addConnectedResponder(Context context, String packageName) {
-        HashSet<String> set = new HashSet<String>(getReceiverPackageNames(context));
-        boolean result = set.add(packageName);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        prefs.edit().putStringSet(PREF_RECEIVER_PACKAGE_NAMES, set).apply();
-        return result;
+        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS, SHARED_PREFS_MODE);
+        // present in the prefs means connected
+        return prefs.edit().putString(packageName, CONNECTED).commit();
     }
 
     /**
@@ -66,18 +69,76 @@ public class PanicTrigger {
      *
      * @param context
      * @param packageName the responder to remove
-     * @return whether it was successfully completed
+     * @return whether it was successfully removed
      */
     public static boolean removeConnectedResponder(Context context, String packageName) {
-        HashSet<String> set = new HashSet<String>(getReceiverPackageNames(context));
-        boolean result = set.remove(packageName);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        prefs.edit().putStringSet(PREF_RECEIVER_PACKAGE_NAMES, set).apply();
-        return result;
+        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS, SHARED_PREFS_MODE);
+        // absent from the prefs means not connected
+        return prefs.contains(packageName) && prefs.edit().remove(packageName).commit();
     }
 
-    public static Set<String> getReceiverPackageNames(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        return prefs.getStringSet(PREF_RECEIVER_PACKAGE_NAMES, Collections.<String> emptySet());
+    /**
+     * Get the {@link Set} of {@code packageNames} of all {@link Activity}s that respond to
+     * {@link Panic#ACTION_TRIGGER}.
+     *
+     * @see #getResponderServices(Context) to get the {@link Service}s
+     */
+    public static Set<String> getResponderActivities(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> activitiesList = pm.queryIntentActivities(PanicUtils.triggerIntent, 0);
+        Set<String> activities = new HashSet<String>();
+        for (ResolveInfo resInfo : activitiesList) {
+            activities.add(resInfo.activityInfo.packageName);
+        }
+        return activities;
+    }
+
+    /**
+     * Get the {@link Set} of {@code packageNames} of all {@link Service}s that respond to
+     * {@link Panic#ACTION_TRIGGER}.
+     *
+     * @see #getResponderActivities(Context) to get the {@link Activity}s
+     */
+    public static Set<String> getResponderServices(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> servicesList = pm.queryIntentServices(PanicUtils.triggerIntent, 0);
+        Set<String> services = new HashSet<String>();
+        for (ResolveInfo resInfo : servicesList) {
+            services.add(resInfo.serviceInfo.packageName);
+        }
+        return services;
+    }
+
+    /**
+     * Get the {@link Set} of all {@code packageNames} of any {@link Activity}s
+     * or {@link Service}s that respond to {@link Panic#ACTION_TRIGGER}
+     * {@link Intent}s.
+     */
+    public static Set<String> getAllResponders(Context context) {
+        List<String> packageNames = new ArrayList<>(getResponderActivities(context));
+        packageNames.addAll(getResponderServices(context));
+        return new HashSet<String>(packageNames);
+    }
+
+    /**
+     * Get the {@link Set} of {@code packageNames} of any {@link Activity}s or
+     * {@link Service}s that respond to {@link Panic#ACTION_TRIGGER} and have
+     * been manually connected by the user to this app.
+     *
+     * @see #checkForConnectIntent(Activity)
+     * @see #checkForDisconnectIntent(Activity)
+     */
+    public static Set<String> getConnectedResponders(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS, SHARED_PREFS_MODE);
+        Set<String> connectedAndInstalled = new HashSet<>();
+        Set<String> all = getAllResponders(context);
+        // present in the prefs means it has been connected
+        for (Map.Entry<String, ?> entry : prefs.getAll().entrySet()) {
+            String packageName = entry.getKey();
+            if (all.contains(packageName)) {
+                connectedAndInstalled.add(packageName);
+            }
+        }
+        return connectedAndInstalled;
     }
 }
